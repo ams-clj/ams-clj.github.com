89142f9ee0a8fa507154d230feec81e279304b74
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-05-11 20:22:04 +0200

    Starting a brand new project: a github robot that spies on you\!

---
+++ src/amsclj_website/core.clj
@@ -0,0 +1,1 @@
+(ns amsclj-website.core)
---
+++ test/amsclj_website/test/core.clj
@@ -0,0 +1,6 @@
+(ns amsclj-website.test.core
+  (:use [amsclj-website.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
