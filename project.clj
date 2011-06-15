be941d3e69d16260484ff302b474df621ea12cb1
Hubert Iwaniuk <neotyk@kungfoo.pl>
2011-06-15 20:42:32 +0200

    update feedparser

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject amsclj-website "1.0.0-SNAPSHOT"
   :description "Amsterdam Clojurians"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [feedparser-clj "0.1"]])
+                 [feedparser-clj "0.2"]])
