fc42faf06456b96cca452ce2b3b4b2dfa1901f1a
Carlo Sciolla <carlo.sciolla@gmail.com>
2012-06-19 20:53:33 +0200

    start really anew

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject amsclj-website "1.0.0-SNAPSHOT"
-  :description "Amsterdam Clojurians"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.0"]
-                 [ring/ring-jetty-adapter "1.1.1"]])
