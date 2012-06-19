82f81166db7ba8d0aaf4b51c2e4e60f69ad868ce
Carlo Sciolla <carlo.sciolla@gmail.com>
2012-06-19 09:00:53 +0200

    Start anew

--- project.clj
+++ project.clj
@@ -1,11 +1,5 @@
 (defproject amsclj-website "1.0.0-SNAPSHOT"
   :description "Amsterdam Clojurians"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [compojure "0.5.3"]
-                 [hiccup "0.3.1"]
-                 [ring/ring-jetty-adapter "0.3.5"]
-                 [ring/ring-devel "0.3.5"]
-                 [feedparser-clj "0.2"]
-                 [org.clojars.nathell/redis-clojure "1.2.7"]
-                 [org.danlarkin/clojure-json "1.1"]
-                 [http.async.client "0.3.1"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.0"]
+                 [ring/ring-jetty-adapter "1.1.1"]])
--- test/amsclj_website/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns amsclj-website.test.core
-  (:use [amsclj-website.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
