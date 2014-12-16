5efb53288aa47789057d4fe8e8148a21fc7cece9
Carlo Sciolla <skuro@skuro.tk>
2014-12-16 15:36:53 +0100

    Twitter column deserves a column header

--- template/index.html.clj
+++ template/index.html.clj
@@ -79,6 +79,7 @@
   ]
 
  [:div {:class "grid_4"}
+  [:h2 "@Amsclj on " [:span {:class "orange"} "Twitter"]]
 
   [:a {:class "twitter-timeline" :href "https://twitter.com/amsclj" :data-widget-id "389666614846582784"} "Tweets by @amsclj"]
   [:script "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');"]
